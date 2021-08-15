027be974d6607701c806aebea7b3fc84be8dedbb
akshay <aksporwal07@gmail.com>
2021-08-15 08:13:18 +0530

    first commit using eclipse

---
+++ src/mypackage/GitTest.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if (hungry=="yes") {
+			System.out.println("eat ");			
+		}else {
+			System.out.println("don't eat");
+			
+		}
+
+	}
+
+}
+ 
